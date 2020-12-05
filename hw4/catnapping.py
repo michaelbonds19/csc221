e732dcbdb6a96db0dc8fa77813c68a201f414e1d
michaelbonds19 <59932724+michaelbonds19@users.noreply.github.com>
2020-12-05 00:47:26 -0500

    Create catnapping.py

---
+++ hw4/catnapping.py
@@ -0,0 +1,13 @@
+catnapping.py:
+ print('''Dear Alice,
+   ...:
+   ...: Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+   ...:
+   ...: Sincerely,
+   ...: Bob''')
+Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob
