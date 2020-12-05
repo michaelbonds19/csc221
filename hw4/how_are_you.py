1f0408e44424946e11985c964028ed0584969b7f
michaelbonds19 <59932724+michaelbonds19@users.noreply.github.com>
2020-12-05 00:48:15 -0500

    Create how_are_you.py

---
+++ hw4/how_are_you.py
@@ -0,0 +1,16 @@
+how_are_you.py
+
+
+In [6]: print('How are you?')
+   ...: feeling = input()
+   ...: if feeling.lower() == 'great':
+   ...:     print('I feel great too.')
+   ...: else:
+   ...:     print('I hope the rest of your day is good.')
+   ...:
+How are you?
+
+I hope the rest of your day is good.
+
+if __name__=="__main__":
+    main()
