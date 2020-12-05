15efa2139cd805198be33f5d911de02e3ab42899
michaelbonds19 <59932724+michaelbonds19@users.noreply.github.com>
2020-12-05 00:52:35 -0500

    Create table_printer.py

---
+++ hw4/table_printer.py
@@ -0,0 +1,9 @@
+''' Table Printer practice project
+
+Author: <Michael Bonds>
+'''
+
+def printTable(tableData):
+   tableData=[['action', 'pear', 'gospel', 'basketball'],
+           ['Michael', 'Tiff', 'Michelle', 'CJ']
+           ['cats', 'bear', 'tiger', 'elephant']
